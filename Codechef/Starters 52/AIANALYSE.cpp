9f692b9419b0b709030dd1d02cfc373393fb57e6
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2022-08-17 20:14:48 +0530

    Create AIANALYSE.cpp

---
+++ Codechef/Starters 52/AIANALYSE.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/START52D/problems/AIANALYSE
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int c;
+    cin >> c;
+
+    if(c <= 1000)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
