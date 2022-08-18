3570877a73204cc7475705f43d99deee1f669a66
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2022-08-18 09:51:45 +0530

    attempt 2

--- Codechef/Starters 52/K-Flip.cpp
+++ Codechef/Starters 52/K-Flip.cpp
@@ -2,18 +2,20 @@
 
 #include <iostream>
 #include <cmath>
+#include "string.h"
 using namespace std;
 
-string s;
+void flip(char *s, int i, int j) {
 
-void flip(int i, int j) {
-
+    // string m = "";
     for (auto k=i; k<=j; k++) {
-        if(s[k])
+        if( s[k] == '1' )
             s[k] = '0';
         else
             s[k] = '1';
     }
+
+    // s = s.substr(0, i) + m + s.substr(j+1);
 }
 
 int main() {
@@ -21,28 +23,47 @@ int main() {
     int t;
     cin >> t;
 
+    // string *ptr = &s;
+
     while (t--)
     {
         int n, k;
         cin >> n >> k;
 
-        cin >> s;
+        char s[n];
+        // cin >> s;
+        for (auto i=0; i<n; i++)
+            cin >> s[i];
+        // string *ptr = &s;
 
-        int upheal = floor((n + k - 1) / 2), downheal = ceil((n + k - 1) / 2);
+        int upheal = floor((1.0)*(n + k - 1) / 2), downheal = ceil((1.0)*(n + k - 1) / 2);
 
-        for (auto i=1; i<=(n-k+1); i++) {
+        for (auto i = 1; i <= upheal; i++) {
 
-            for (auto j=0; j<s.length(); j++) {
+                for (auto j=0; j<n; j++) {
 
-                if ()
-
-                if(s[j] == 0) {
-                    flip(j, j+k-1);
-                    i++;
+                    if(s[j] == '0') {
+                        flip( &s[0], j, j+k-1);
+                        // i++;
+                        break;
+                    }
                 }
-            }
-            
         }
+        for (auto i = upheal+1; i <= upheal+downheal; i++) {
+
+            for (auto j=0; j<n; j++) {
+
+                    if(s[j] == '1') {
+                        flip( &s[0], j, j+k-1);
+                        // i++;
+                        break;
+                    }
+            }
+        }
+
+        for (auto i=0; i<n; i++)
+            cout << s[i];
+        cout << endl;
     }
     
 }
